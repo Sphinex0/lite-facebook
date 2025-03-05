28f3e855bb44e1ff32da213363025a4ab0f62522
killux <killux>
2025-03-05 13:23:41 +0000

    mix mix

--- backend/internal/repository/reset.sql
+++ backend/internal/repository/reset.sql
@@ -1,13 +1,12 @@
 INSERT INTO users (
     nickname,
-    datebirth,
+    date_birth,
     first_name,
     last_name,
     email,
     password,
-    uuid,
-    uuid_exp,
-    image
+    image,
+    created_at
 ) VALUES (
     'testuser',
     952259471,
@@ -15,7 +14,17 @@ INSERT INTO users (
     'Doe',
     'john.doe@example.com',
     'hashed_password123',
-    '550e8400-e29b-41d4-a716-446655440000',
-    1677654321,
-    'profile.jpg'
+    'profile.jpg',
+    952259471
 );
+
+INSERT INTO sessions VALUES (
+    NULL,
+    1,
+    '550e8400-e29b-41d4-a716-446655440000',
+    1677654321
+)
+
+
+    -- '550e8400-e29b-41d4-a716-446655440000',
+    -- 1677654321,
