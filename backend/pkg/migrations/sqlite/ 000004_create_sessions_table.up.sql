dc20ba7e00e888634d2ab6ede1a3e25ec0a20270
aelmir <adnane.elmir1@gmail.com>
2025-03-05 13:18:05 +0000

    session id type modification

--- backend/pkg/migrations/sqlite/ 000004_create_sessions_table.up.sql
+++ backend/pkg/migrations/sqlite/ 000004_create_sessions_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE
     sessions (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER NOT NULL,
-        session_id INTEGER NOT NULL,
+        session_id TEXT NOT NULL,
         session_exp INTEGER NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
     );
