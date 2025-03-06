3f558a9cac08eeb2c3efaf4ac6cf83522b0dd1fd
Your Name <you@example.com>
2025-03-06 12:36:31 +0000

    done

--- backend/internal/api/handler/groupHandler.go
+++ backend/internal/api/handler/groupHandler.go
@@ -1,8 +1,32 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"social-network/internal/models"
+	utils "social-network/pkg"
+	"social-network/pkg/middlewares"
+)
 
 func (Handler *Handler) AddGroup(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.WriteJson(w, http.StatusMethodNotAllowed, "method not allowed")
+		return
+	}
+	User, ok := r.Context().Value(middlewares.UserIDKey).(models.UserInfo)
+	if !ok {
+		utils.WriteJson(w, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+	var Group models.Group
+	Group.Creator = User.ID
+	Group.Title = strings.TrimSpace(r.FormValue("Title"))
+	Group.Description = strings.TrimSpace(r.FormValue("Description"))
+	Group.CreatedAt=int(time.Now().Unix())
+	
+	
 }
 
 func (Handler *Handler) GetGroups(w http.ResponseWriter, r *http.Request) {
---
+++ backend/internal/service/Group.go
@@ -0,0 +1,8 @@
+package service
+
+import "social-network/internal/models"
+
+
+func (S *Service) GetGroup(User *models.User) error {
+	
+}
