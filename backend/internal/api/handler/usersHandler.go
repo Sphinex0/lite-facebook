386a0752bd7ed3e33308570d2014145891a078df
aelmir <adnane.elmir1@gmail.com>
2025-03-18 11:43:38 +0000

    fix img src

--- backend/internal/api/handler/usersHandler.go
+++ backend/internal/api/handler/usersHandler.go
@@ -87,4 +87,40 @@ func (Handler *Handler) HandleGetProfileAbout(w http.ResponseWriter, r *http.Req
 }
 
 func (Handler *Handler) HandleUpdateProfile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.WriteJson(w, http.StatusMethodNotAllowed, "method not allowed")
+		return
+	}
+
+	user, ok := r.Context().Value(utils.UserIDKey).(models.UserInfo)
+	if !ok {
+		utils.WriteJson(w, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+
+	var profile models.User
+	var err error
+
+	err = utils.ParseBody(r, &profile)
+	if err != nil {
+		log.Println(err)
+		utils.WriteJson(w, http.StatusBadRequest, "Bad request")
+		return
+	}
+	if profile.ID == 0 {
+		profile.ID = user.ID
+	}
+
+	err = Handler.Service.GetProfile(&profile, user.ID)
+	if err != nil {
+		log.Println(err)
+		if err.Error() == "profile is private, follow to see"{
+			utils.WriteJson(w, http.StatusForbidden, http.StatusText( http.StatusForbidden))
+			return
+		}
+		utils.WriteJson(w, http.StatusBadRequest, "Bad request")
+		return
+	}
+
+	utils.WriteJson(w, http.StatusOK, profile)
 }
