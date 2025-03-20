af819117fd0c3c8115cf40c3260479e8724ad83a
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-03-20 21:17:59 +0000

    add member

---
+++ backend/internal/repository/members.go
@@ -0,0 +1,9 @@
+package repository
+
+func (data *Database) SaveMember(member models.Member) (err error) { 
+	args := utils.GetExecFields(member, "ID")
+    _, err := data.Db.Exec(fmt.Sprintf(`
+        INSERT INTO members
+        VALUES (NULL, %v)
+    `, utils.Placeholders(len(args))), args...)                                                                                            return
+}
