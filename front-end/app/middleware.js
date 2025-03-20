b20a9f9abf47584ba9dab77ccc40df28721b17ed
killux <killux>
2025-03-20 16:44:19 +0000

    fix chat and context

--- front-end/app/middleware.js
+++ front-end/app/middleware.js
@@ -1,26 +1,39 @@
-import { NextResponse} from "next/server";
+// import { NextResponse} from "next/server";
 
-async function checkuservalidity(req) {
-  const response = await fetch(`${window.location.host}/api/checkuser`,{
-    headers: {
-      cookie: req.headers.get("session_token") || "",
-    },
-  })
-  
-  return response.status == 200
-}
+// async function checkuservalidity(req) {
+//   const response = await fetch(`${window.location.host}/api/checkuser`,{
+//     headers: {
+//       cookie: req.headers.get("session_token") || "",
+//     },
+//   })
 
-export async function middleware(req) {
- // const authToken =  await checkuservalidity(req)
-//console.log("sfsgdgdfgdfg");
+//   return response.status == 200
+// }
 
-  if (req.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+// export async function middleware(req) {
+//  // const authToken =  await checkuservalidity(req)
+// //console.log("sfsgdgdfgdfg");
 
-}
+//   if (req.nextUrl.pathname === "/") {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+
+// }
 /*
 export const config = {
   matcher: ["/"],
 };
 */
+
+// import { NextResponse } from 'next/server';
+
+// export default async function middleware(request) {
+//   console.log("Middleware running for:", request.nextUrl.pathname); // Log the pathname
+//   const response = await NextResponse.next();
+//   console.log("Response status:", response.status); // Log the status
+//   if (response.status === 404) {
+//     console.log("Redirecting to /login");
+//     return NextResponse.redirect(new URL('/login', request.url));
+//   }
+//   return response;
+// }
