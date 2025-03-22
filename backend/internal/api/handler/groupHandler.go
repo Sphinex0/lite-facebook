c2fc5103999cd1938b180afc933f6b051d461ef0
killux <killux>
2025-03-22 15:21:31 +0000

    gg

--- backend/internal/api/handler/groupHandler.go
+++ backend/internal/api/handler/groupHandler.go
@@ -26,7 +26,6 @@ func (Handler *Handler) AddGroup(w http.ResponseWriter, r *http.Request) {
 	Group.Creator = user.ID
 	Group.Title = strings.TrimSpace(r.FormValue("Title"))
 	Group.Description = strings.TrimSpace(r.FormValue("Description"))
-<<<<<<< HEAD
 	err := r.ParseMultipartForm(10 << 20)
 	if err != nil {
 		utils.WriteJson(w, http.StatusBadRequest, "file too big")
@@ -43,12 +42,7 @@ func (Handler *Handler) AddGroup(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	Group.CreatedAt = int(time.Now().Unix())
-=======
 	Group.CreatedAt = int(time.Now().UnixMilli())
-	fmt.Println(Group.Title)
-	fmt.Println(Group.Description)
->>>>>>> 0acfa975dd9832608db6324ad6be2153af3a3e33
 	if Group.Title == "" || len(Group.Title) > 50 || Group.Description == "" || len(Group.Description) > 250 {
 		utils.WriteJson(w, http.StatusBadRequest, http.StatusText(http.StatusInternalServerError))
 		return
